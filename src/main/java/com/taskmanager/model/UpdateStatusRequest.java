61adc1518d7b11f03af7555085e34dc5ea3255b4
Steven Tran <stevent925@gmail.com>
2025-03-27 02:18:24 -0500

    Controller, services, repository foundation

---
+++ src/main/java/com/taskmanager/model/UpdateStatusRequest.java
@@ -0,0 +1,12 @@
+package com.taskmanager.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateStatusRequest {
+    private TaskStatus status;
+}
